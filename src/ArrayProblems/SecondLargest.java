ebb597555abb367bef42ba699299a90c4d1a8ac2
bhavanach-deve <bhavanachilkeshwaram@gmail.com>
2026-07-20 15:57:35 +0530

    Arrays

---
+++ src/ArrayProblems/SecondLargest.java
@@ -0,0 +1,27 @@
+package ArrayProblems;
+//Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
+//Note: The second largest element should not be equal to the largest element.
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class SecondLargest
+{
+    public static int getSecondLargest(int arr[])
+    {
+        Arrays.sort(arr);
+        int largest=arr[arr.length-1];
+        for(int i=arr.length-2;i>=0;i--){
+            if(arr[i]!=largest){
+                return arr[i];
+            }
+        }
+        return -1;
+
+    }
+
+    public static void main(String[] args)
+    {
+        int arr[]={10,10,5};
+        System.out.println(getSecondLargest(arr));
+    }
+}
