c5b83f23777f283362c371df372d48b44a6e3e97
bhavanach-deve <bhavanachilkeshwaram@gmail.com>
2026-07-31 22:10:05 +0530

    Arrays-Day5 Add program to rotate an image by 90 clock-wise

---
+++ src/ArrayProblems/Day5/RotateImage.java
@@ -0,0 +1,39 @@
+package ArrayProblems.Day5;
+
+public class RotateImage
+{
+    public static void rotate(int [][]mat) {
+        int n = mat.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            int left=0;
+            int right=n-1;
+            while(left<right) {
+                int temp = mat[i][left];
+                mat[i][left] = mat[i][right];
+                mat[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int mat[][]={{0,1,2},{3,4,5},{6,7,8}};
+        rotate(mat);
+    }
+}
